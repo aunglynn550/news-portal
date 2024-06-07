11e1336534db9095afcc8fa323095c90d68c1bbe
aunglynn550 <aunglynn550@gmail.com>
2024-06-07 15:42:29 +0630

    admin dashboard segmentation

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,42 @@
+<div class="navbar-bg"></div>
+<!-- Navbar Start -->
+@include('admin.layouts.navbar')
+<!-- Navbar End -->
+<div class="main-sidebar sidebar-style-2">
+  <aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">Stisla</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">St</a>
+    </div>
+    <ul class="sidebar-menu">
+      <li class="menu-header">Dashboard</li>
+      <li class="active">
+        <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>        
+      </li>
+      <li class="menu-header">Starter</li>
+     
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+          <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+          <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+        </ul>
+      </li>
+      <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+     
+     
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
+          <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
+          <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
+        </ul>
+      </li>
+    
+    </ul>
+
+  </aside>
+</div>
