2475d3062f30b81c67a06e7b91cf5ff1900662e0
aunglynn550 <aunglynn550@gmail.com>
2024-06-08 20:05:03 +0630

    setting up multi authentication for admin Part-II

--- app/Http/Controllers/Admin/AdminAuthenticationController.php
+++ app/Http/Controllers/Admin/AdminAuthenticationController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\HandleLoginRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAuthenticationController extends Controller
 {
     public function login()
     {
         return view('admin.auth.login');
+    }//end method
+    public function handleLogin(HandleLoginRequest $request)
+    {
+        $request->authenticate();
+
+        return redirect()->route('admin.dashboard');
+    }//end method
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login');
     }
 }
